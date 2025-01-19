e908ff057f21e11bab3e29905d1ca389a10b38c4
Ale083 <alejandro.umana.miranda@gmail.com>
2025-01-18 21:10:59 -0600

    Optimal 56%

--- 3_LongestSubstringWithoutRepeatingCharacters/main.cpp
+++ 3_LongestSubstringWithoutRepeatingCharacters/main.cpp
@@ -2,58 +2,24 @@
 using namespace std;
 #include <unordered_map>
 
-//can be more efficient by using a sliding window approach, this is by
-//having the hashmap be of char, last index. and saving a start and end of longest substring (window). 
-
 int lengthOfLongestSubstring(string s) {
-    unordered_map<char, bool> map;
+    unordered_map<char, int> map;
     int maxLength = 0;
-    int currentLength = 0;
+    int start = 0;
     for(int i=0; i<s.size();i++) {
-        if(map[s[i]]) {
-            if(currentLength > maxLength) {
-                maxLength = currentLength;
-            }
-            currentLength = 0;
-            map.clear();
-            char dup = s[i--];
-            while(s[i] != dup) {
-                i--; //go back to the last character until it finds the index of the one duplicated.
-            }
-        } else {
-            currentLength++;
-            map[s[i]] = true;
+        if(map.find(s[i]) != map.end() && map[s[i]] >= start) {
+            start = map[s[i]] + 1;
+        }
+        map[s[i]] = i;
+        if(i-start+1 > maxLength) {
+            maxLength = i-start+1;
         }
     }
-
-    if(currentLength > maxLength) {
-        maxLength = currentLength;
-    }
-
     return maxLength;
 }
+
 int main() {
     cout<< lengthOfLongestSubstring("abcabcbaed") << endl;
 
     return 0;
 }
-
-
-/*
-*for(char c : s) {
-        if(map[c]) {
-            cout << "maxLength: " << maxLength << " currentLength: " << currentLength << endl;
-            if(currentLength > maxLength) {
-                maxLength = currentLength;
-            }
-            currentLength = 1;
-            map.clear();
-            map[c] = true;
-        } else {
-            currentLength++;
-            cout << "currentLength: " << currentLength << endl;
-            map[c] = true;
-        }
-    }
- *
- */
